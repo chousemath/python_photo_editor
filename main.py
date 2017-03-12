fe931cd377e76bb6b23a5d7c607b772c790d5afb
Joseph Sungpil Choi <chousemath@gmail.com>
2017-03-12 13:14:11 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter
+
+# Some general setup code for the entire app
+window = tkinter.Tk()
+window.title("Python Photo Editor")
+window.geometry("500x500")
+
+# Code to add tkinter widgets will go here
+window.mainloop()
