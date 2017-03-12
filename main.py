6b2d4ec300dda070ddb98e8d8971b98d79b54ad4
Joseph Sungpil Choi <chousemath@gmail.com>
2017-03-12 13:24:31 +0900

    got so far as to simply show an image inside the tkinter frame

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import tkinter
+from tkinter import Label
+from PIL import Image, ImageFilter, ImageTk
 
 # Some general setup code for the entire app
 window = tkinter.Tk()
@@ -6,4 +8,12 @@ window.title("Python Photo Editor")
 window.geometry("500x500")
 
 # Code to add tkinter widgets will go here
+
+# image import code
+test_image = Image.open('test1.jpg')
+test_photo = ImageTk.PhotoImage(test_image)
+test_photo_label = Label(image=test_photo)
+test_photo_label.image = test_photo
+test_photo_label.pack()
+
 window.mainloop()
